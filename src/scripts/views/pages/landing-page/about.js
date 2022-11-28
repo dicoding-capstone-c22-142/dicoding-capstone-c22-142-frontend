d8c8e550b4ffba81b18c5d2c61cc70cc7a6a8949
Gregorius Damar <damargregorius1002@gmail.com>
2022-11-28 23:24:26 +0700

    add content

--- src/scripts/views/pages/landing-page/about.js
+++ src/scripts/views/pages/landing-page/about.js
@@ -6,9 +6,28 @@ const About = {
   async afterRender() {
     const wrapper = document.querySelector('.wrapper');
     wrapper.innerHTML = `
-    <div class="d-flex align-items-center" style="height: 100vh; background-color: steelblue">
-        <h1>ini halaman about</h1>
-    </div>`;
+    <div class="d-flex flex-column" style="height: 100vh; background-color: steelblue">
+        <div class="about p-2" style="margin-top:10vh">
+        <h1>About</h1>
+        <p>Cashtex merupakan aplikasi kasir yang memudahkan toko tekstil untuk bertransaksi dengan pelanggannya</p>
+        </div>
+
+        <div class="features p-2">
+        <h1>Features</h1>
+        <div class = "p-2">
+        <h3>Dashboard</h3>
+        <p>Anda dapat dengan mudah mengatur pengguna dan daftar transaksi dengan halaman dashboard</p>
+        </div>
+        <div class = "p-2">
+        <h3>Report</h3>
+        <p>Pantau hasil penjualan anda dengan halaman Report dengan jangka waktu yang dapat anda tentukan sendiri</p>
+        </div>
+        <div class = "p-2">
+        <h3>Transaction history</h3>
+        <p>Segala transaksi akan secara otomatis tercatat di halaman Transaction history</p>
+        </div>
+  </div>
+</div>`;
   },
 };
 
