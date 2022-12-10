6765b0b7a1e6c4696857deb68def0207cb36a205
Gregorius Damar <damargregorius1002@gmail.com>
2022-12-10 19:54:29 +0700

    update landing page and insert footer-bar

--- src/scripts/components/header-bar.js
+++ src/scripts/components/header-bar.js
@@ -9,7 +9,6 @@ class HeaderBar extends HTMLElement {
       <h1 class="fw-bolder">Welcome To CashTex</h1>
       <p>Pilihan terbaik untuk mengembangkan bisnis anda. Gunakan Cashtex agar anda dapat dengan mudah untuk melakukan transaksi dengan pelanggan.</p>
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Sign In</button>
-        <a href="/registration">Sign Up</a>
       </div>
       <div class="header-img">
         <picture>
