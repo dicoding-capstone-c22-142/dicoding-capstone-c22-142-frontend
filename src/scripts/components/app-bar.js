786af39ded801226d200443858c634a5193e50ca
Gregorius Damar <damargregorius1002@gmail.com>
2022-12-10 16:09:48 +0700

    add our team section and add image file

--- src/scripts/components/app-bar.js
+++ src/scripts/components/app-bar.js
@@ -8,8 +8,9 @@ class AppBar extends HTMLElement {
       <div><a href="/"><h2>CashTex</h2></a></div>
       <div class="drawer">
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#/about">About Us</a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Cashtex</a></li>
+            <li><a href="#team">Our Team</a></li>
           </ul>
           <div class="registration">
             <button type="button" class="btn btn-primary btn-login" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
